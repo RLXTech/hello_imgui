a18a6d15ecfa5d6b2c5fc7ddef65c06e66d31a66
zaj <1418623546@qq.com>
2025-04-07 11:46:39 +0800

    feat: update

--- src/hello_imgui/impl/imgui_default_settings.cpp
+++ src/hello_imgui/impl/imgui_default_settings.cpp
@@ -1,89 +1,88 @@
 #include "hello_imgui/imgui_default_settings.h"
+#include "hello_imgui/hello_imgui.h"
 #include "hello_imgui/hello_imgui_assets.h"
 #include "hello_imgui/hello_imgui_font.h"
-#include "hello_imgui/hello_imgui.h"
-
 
 namespace HelloImGui
 {
 
-
 namespace ImGuiDefaultSettings
 {
 
-
-void LoadDefaultFont_WithFontAwesomeIcons()
-{
-	auto runnerParams = HelloImGui::GetRunnerParams();
-    auto defaultIconFont = runnerParams->callbacks.defaultIconFont;
-    float fontSize = 15.f;
-
-    std::string fontFilename = "fonts/DroidSans.ttf";
-
-    if ( ! HelloImGui::AssetExists(fontFilename))
+    void LoadDefaultFont_WithFontAwesomeIcons()
     {
-        ImGui::GetIO().Fonts->AddFontDefault();
-        return;
+        auto runnerParams = HelloImGui::GetRunnerParams();
+        auto defaultIconFont = runnerParams->callbacks.defaultIconFont;
+        float fontSize = 15.f;
+
+        std::string fontFilename = "fonts/SourceHanSansCN-Medium.otf";
+
+        if (!HelloImGui::AssetExists(fontFilename))
+        {
+            ImGui::GetIO().Fonts->AddFontDefault();
+            return;
+        }
+
+        bool useDpiResponsiveFonts = runnerParams->dpiAwareParams.onlyUseFontDpiResponsive;
+        HelloImGui::FontLoadingParams fontDefaultParams;
+        fontDefaultParams.useFullGlyphRange = true;
+        if (useDpiResponsiveFonts)
+            LoadFontDpiResponsive(fontFilename, fontSize, fontDefaultParams);
+        else
+            LoadFont(fontFilename, fontSize, fontDefaultParams);
+
+        if (defaultIconFont == HelloImGui::DefaultIconFont::NoIcons)
+            return;
+
+        std::string iconFontFile;
+        if (defaultIconFont == HelloImGui::DefaultIconFont::FontAwesome4)
+            iconFontFile = "fonts/Font_Awesome_5_Free-Solid-900.otf";
+        else if (defaultIconFont == HelloImGui::DefaultIconFont::FontAwesome6)
+            iconFontFile = "fonts/Font_Awesome_6_Free-Solid-900.otf";
+        else
+            return;
+
+        if (!HelloImGui::AssetExists(iconFontFile))
+            return;
+
+        HelloImGui::FontLoadingParams fontParams;
+        fontParams.mergeToLastFont = true;
+        fontParams.useFullGlyphRange = true;
+
+        if (useDpiResponsiveFonts)
+            LoadFontDpiResponsive(iconFontFile, fontSize, fontParams);
+        else
+            LoadFont(iconFontFile, fontSize, fontParams);
     }
 
-	bool useDpiResponsiveFonts = runnerParams->dpiAwareParams.onlyUseFontDpiResponsive;
-
-	if (useDpiResponsiveFonts)
-    	LoadFontDpiResponsive(fontFilename, fontSize);
-	else
-		LoadFont(fontFilename, fontSize);
-
-	if (defaultIconFont == HelloImGui::DefaultIconFont::NoIcons)
-        return;
-
-    std::string iconFontFile;
-    if (defaultIconFont == HelloImGui::DefaultIconFont::FontAwesome4)
-        iconFontFile = "fonts/fontawesome-webfont.ttf";
-    else if (defaultIconFont == HelloImGui::DefaultIconFont::FontAwesome6)
-        iconFontFile = "fonts/Font_Awesome_6_Free-Solid-900.otf";
-    else
-        return;
-
-    if ( ! HelloImGui::AssetExists(iconFontFile))
-        return;
-
-    HelloImGui::FontLoadingParams fontParams;
-    fontParams.mergeToLastFont = true;
-    fontParams.useFullGlyphRange = true;
-
-	if (useDpiResponsiveFonts)
-    	LoadFontDpiResponsive(iconFontFile, fontSize, fontParams);
-	else
-		LoadFont(iconFontFile, fontSize, fontParams);
-}
-
-void SetupDefaultImGuiConfig()
-{
-    // Setup Dear ImGui context
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Docking
-    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-
-    // io.ConfigViewportsNoAutoMerge = true;
-    // io.ConfigViewportsNoTaskBarIcon = true;
-}
-
-void SetupDefaultImGuiStyle()
-{
-    // Note: a theme was already applied via ImGuiWindowParams.tweakedTheme
-
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows
-    // can look identical to regular ones.
-    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    void SetupDefaultImGuiConfig()
     {
-        ImGuiStyle& style = ImGui::GetStyle();
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        // Setup Dear ImGui context
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Docking
+        // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform
+        // Windows
+
+        // io.ConfigViewportsNoAutoMerge = true;
+        // io.ConfigViewportsNoTaskBarIcon = true;
     }
-}
+
+    void SetupDefaultImGuiStyle()
+    {
+        // Note: a theme was already applied via ImGuiWindowParams.tweakedTheme
+
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows
+        // can look identical to regular ones.
+        if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            ImGuiStyle& style = ImGui::GetStyle();
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+    }
 
 }  // namespace ImGuiDefaultSettings
 }  // namespace HelloImGui
